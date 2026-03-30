77757c3f12313e6a2ca7486ddd284ea9323a994a
jangsunwoo <sun475300@gmail.com>
2026-03-30 17:03:34 +0900

    Phase 199-200: C# v3

---
+++ csharp3/BattleSim.cs
@@ -0,0 +1,6 @@
+// Phase 200: C# v3
+using System;
+class Program {
+    static int CalculateSwarmDamage(int count) => count * 5;
+    static void Main() => Console.WriteLine(CalculateSwarmDamage(10));
+}
